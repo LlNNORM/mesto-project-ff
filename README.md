f40cb6818062f30196e2d0d58fd25573cba87768
Ivan <linnorm@ya.ru>
2024-09-20 13:06:35 +0300

    Rendering for cards from array implemented

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Проектная работа Mesto
+# Project Mesto
+
+### MY FIFTH PROJECT AT YANDEX PRACTICUM:
+### https://github.com/LlNNORM/mesto-project-ff.git
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,18 @@
-// @todo: Темплейт карточки
-const cardTemplate = document.querySelector('#card-template').content;
-const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector("#card-template").content;
+const placesList = document.querySelector(".places__list");
 
+initialCards.forEach(({ link, name }) => renderCard(link, name, deleteCard));
 
+function renderCard(src, cardTitle, deleteFunction) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  cardElement.querySelector(".card__image").src = src;
+  cardElement.querySelector(".card__title").textContent = cardTitle;
+  deleteButton.addEventListener("click", () => deleteFunction(deleteButton));
 
-
-{/* <template id="card-template">
-  <li class="places__item card">
-    <img class="card__image" src="" alt="" />
-    <button type="button" class="card__delete-button"></button>
-    <div class="card__description">
-      <h2 class="card__title">
-      </h2>
-      <button type="button" class="card__like-button"></button>
-    </div>
-  </li>
-</template> */}
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-function createCard(imageSrc, cardTitle, deleteFunction) { 
-    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    cardElement.querySelector('.card__image').src = imageSrc;
-    cardElement.querySelector('.card__title').textContent = cardTitle;
-    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteFunction);
-
-    return placesList.append(cardElement); 
+  return placesList.append(cardElement);
 }
 
-// @todo: Функция удаления карточки
-function deleteCard() { 
-    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteFunction);
+function deleteCard(deleteButton) {
+  deleteButton.closest(".card").remove();
 }
-// @todo: Вывести карточки на страницу
-createCard('../images/waterfall.webp', 'Водопады Игуасу')
