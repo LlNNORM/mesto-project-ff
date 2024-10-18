33357b88bfed145a8658841857d903f88abf443d
Ivan <linnorm.ya.ru>
2024-10-18 23:47:58 +0300

    Completed script before module structure implementation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,35 +6,38 @@ const cardsContainer = document.querySelector(".places__list");
 const profileEditButton = document.querySelector(".profile__edit-button");
 const placesAddButton = document.querySelector(".profile__add-button");
 const popupCloseButtons = document.querySelectorAll(".popup__close");
+const popups = document.querySelectorAll(".popup");
 const profileEditPopup = document.querySelector(".popup_type_edit");
 const placeAddPopup = document.querySelector(".popup_type_new-card");
 const imagePopup = document.querySelector(".popup_type_image");
 
 renderCards(initialCards);
 
-profileEditButton.addEventListener('click', (e) => { 
-  openPopup(profileEditPopup);
-});
+popups.forEach(popup => popup.classList.add('popup_is-animated'));
+
+profileEditButton.addEventListener('click', () => openPopup(profileEditPopup));
 placesAddButton.addEventListener('click', () => openPopup(placeAddPopup));
-popupCloseButtons.forEach(button => button.addEventListener('click', (evt) => closePopupByCross(evt.target)));
+popupCloseButtons.forEach(button => button.addEventListener('click', closePopupByCross));
 
 function renderCards (cardList) {
   cardList.forEach((cardData)=> {
-    const card = createCard(cardData, deleteCard, openPopup);
+    const card = createCard(cardData, deleteCard, likeCard, openImagePopup);
     return cardsContainer.append(card);
   });
 }
 
-function createCard(cardData, deleteCardFunction, openPopupFunction) {
+function createCard(cardData, deleteCardFunction, likeCardFunction, openPopupFunction) {
   const card = cardTemplate.querySelector(".card").cloneNode(true);
   const deleteButton = card.querySelector(".card__delete-button");
+  const likeButton = card.querySelector(".card__like-button");
   const cardImage = card.querySelector(".card__image");
   const cardTitle = card.querySelector(".card__title")
   cardImage.src = cardData['link'];
   cardImage.alt = cardData['alt'];
-  cardImage.addEventListener('click', () => openPopupFunction(imagePopup));
   cardTitle.textContent = cardData['name'];
+  cardImage.addEventListener('click', (evt) => openPopupFunction(evt.target, cardTitle.textContent));
   deleteButton.addEventListener("click", () => deleteCardFunction(card));
+  likeButton.addEventListener("click", () => likeCardFunction(likeButton));
   return card
 }
 
@@ -42,22 +45,43 @@ function deleteCard(card) {
   card.remove();
 }
 
-function openPopup(popupType) {
-  popupType.classList.add('popup_is-opened');
-  popupType.addEventListener('click', (evt)=> evt.target===evt.currentTarget ? closePopup(popupType): undefined);
-  document.addEventListener('keydown', (evt)=>  {evt.key==='Escape' ? closePopup(popupType): undefined
-    console.log('esc')
-  }
-  );
+function likeCard(likeButton) {
+  likeButton.classList.toggle('card__like-button_is-active')
 }
 
-function closePopupByCross(crossButton) {
-  crossButton.closest('.popup').classList.remove('popup_is-opened')
+function openPopup(popupType) {
+  popupType.classList.add('popup_is-opened');
+  popupType.addEventListener('click', closePopupByOverlay);
+  document.addEventListener('keydown', closePopupByEsc);
+}
+
+function openImagePopup(target, title) {
+  openPopup(imagePopup);
+  const image =  document.querySelector('.popup__image');
+  const imageDescription =  document.querySelector('.popup__caption');
+  image.src = target.src;
+  image.alt = target.alt;
+  imageDescription.textContent = title;
+  
+}
+
+function closePopupByCross(evt) {
+  evt.target.closest('.popup').classList.remove('popup_is-opened')
+}
+
+function closePopupByEsc(evt) {
+  const openedPopup=document.querySelector('.popup_is-opened');
+  evt.key==='Escape' ? closePopup(openedPopup): undefined
+}
+
+function closePopupByOverlay(evt) {
+  const openedPopup=document.querySelector('.popup_is-opened');
+  evt.target===evt.currentTarget ? closePopup(openedPopup): undefined
 }
 
 function closePopup (popup) {
   popup.classList.remove('popup_is-opened');
-  document.removeEventListener('keydown', closePopup);
+  document.removeEventListener('keydown', closePopupByEsc);
 }
 
 const profileEditFormElement = document.forms.editProfile
